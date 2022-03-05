15d70b6fa8b5aef9816e9a032ab0da2907ca08ec
Santosh Shinde <santoshshinde2012@gmail.com>
2022-03-05 21:48:11 +0530

    fixed production build related issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles/index.scss";
 import store from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
