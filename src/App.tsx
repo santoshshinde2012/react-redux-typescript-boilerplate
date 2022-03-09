defb4de1309df9b31b624821a8f501999653e520
Santosh Shinde <santoshshinde2012@gmail.com>
2022-03-09 14:03:19 +0530

    added react router dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage></LandingPage>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path={"/dashboard"} element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
